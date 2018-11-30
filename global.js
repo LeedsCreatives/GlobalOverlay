ebe2445ed7c03b83dea210f631cf93652621bb0c
Jack Biggin <me@jackbiggin.me>
2018-11-29 23:32:24 -0500

    Top corner branding + basic overlay (unfinished)

---
+++ global.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+    // Top Corner Branding
+    topCorner = ""
+    topCorner += '<div class="lc-branding-bar">'
+    topCorner +=    '<div class="lc-corner-branding">'
+    topCorner +=        '<img src="http://www.thisislsr.com/wp-content/uploads/2018/10/logo.png" /> Part of Leeds Creatives'
+    topCorner +=    '</div>'
+
+    topCorner +=    '<div class="lc-corner-sitelist">'
+
+    topCorner +=        '<div class="lc-corner-site bg-lc">'
+    topCorner +=            '<a href="http://thisislsr.com"><img src="http://www.thisislsr.com/wp-content/uploads/2018/10/logo.png" /> LeedsCreatives.com</a>'
+    topCorner +=        '</div>'
+
+    topCorner +=        '<div class="lc-corner-site bg-lsr">'
+    topCorner +=            '<a href="http://thisislsr.com"><img src="/img/logo-lsr.png" /> Leeds Student Radio</a>'
+    topCorner +=        '</div>'
+
+    topCorner +=        '<div class="lc-corner-site bg-lstv">'
+    topCorner +=            '<a href="http://thisislsr.com"><img src="http://www.thisislsr.com/wp-content/uploads/2018/10/logo.png" /> Leeds Student Television</a>'
+    topCorner +=        '</div>'
+
+
+    topCorner +=        '<div class="lc-corner-site bg-gryphon">'
+    topCorner +=            '<a href="http://thisislsr.com"><img src="http://www.thisislsr.com/wp-content/uploads/2018/10/logo.png" /> The Gryphon</a>'
+    topCorner +=        '</div>'
+
+    topCorner +=        '<div class="lc-corner-site bg-nc">'
+    topCorner +=            '<a href="http://thisislsr.com"><img src="./img/logo-nc.png" /> The Northern Connection</a>'
+    topCorner +=        '</div>'
+
+    topCorner +=    '</div>'
+    topCorner += '</div>'
+
+    $('body').prepend(topCorner);
+});
